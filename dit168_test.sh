a65eb3fefc2fa591582839e1bffa1d2c9cedf9fd
Andreas Fransson <ankanx@gmail.com>
2017-01-24 19:27:37 +0100

    update. tested. works

--- dit168_test.sh
+++ dit168_test.sh
@@ -5,30 +5,32 @@
 
 echo "Starting Test setup...\n"
 echo "Getting DIT168 test package...\n"
-sudo cd && wget http://www.cse.chalmers.se/~bergerc/DIT-168.zip
+sudo mkdir DIT168-test
+sudo cd DIT168-test
+sudo wget http://www.cse.chalmers.se/~bergerc/DIT-168.zip
 echo "Decompressing and moving into test package....\n"
-sudo cd && unzip DIT-168.zip
+sudo unzip DIT-168.zip
 
 
 echo "Pulling latest docker  opendavinci-ubuntu-16.04....\n "
 sudo docker pull seresearch/opendavinci-ubuntu-16.04-complete:latest
 
 echo "Setting up test enviorment...\n"
-sudo docker run -ti --rm --net=host -v $HOME/DIT-168:/opt/configuration -w /opt/configuration seresearch/opendavinci-ubuntu-16.04-complete:latest /opt/od4/bin/odsupercomponent --cid=111 --verbose=1
+sudo gnome-terminal --tab -e "sudo docker run -ti --rm --net=host -v $HOME/DIT-168:/opt/configuration -w /opt/configuration seresearch/opendavinci-ubuntu-16.04-complete:latest /opt/od4/bin/odsupercomponent --cid=111 --verbose=1"
 
+sudo gnome-terminal --tab -e "docker run -ti --rm --net=host seresearch/opendavinci-ubuntu-16.04-complete:latest /opt/od4/bin/odsimvehicle --cid=111 --freq=10"
 
-
-sudo docker run -ti --rm --net=host seresearch/opendavinci-ubuntu-16.04-complete:latest /opt/od4/bin/odsimvehicle --cid=111 --freq=10
-
-sudo docker run -ti --rm --net=host -v $HOME/DIT-168:/opt/configuration -w /opt/configuration seresearch/opendavinci-ubuntu-16.04-complete:latest /opt/od4/bin/odsimirus --cid=111 --freq=10
+sudo gnome-terminal --tab -e "docker run -ti --rm --net=host -v $HOME/DIT-168:/opt/configuration -w /opt/configuration seresearch/opendavinci-ubuntu-16.04-complete:latest /opt/od4/bin/odsimirus --cid=111 --freq=10"
 
 sudo xhost +
 
-sudo docker run -ti --rm --net=host --ipc=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/DIT-168:/opt/configuration -w /opt/configuration seresearch/opendavinci-ubuntu-16.04-complete:latest/ opt/od4/bin/odcockpit --cid=111
+sudo gnome-terminal --tab -e "docker run -ti --rm --net=host --ipc=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/DIT-168:/opt/configuration -w /opt/configuration seresearch/opendavinci-ubuntu-16.04-complete:latest /opt/od4/bin/odcockpit --cid=111"
 
-sudo docker run -ti --rm --net=host seresearch/opendavinci-ubuntu-16.04-complete:latest /opt/od4/bin/miniature/boxparker --cid=111 --freq=10
+sudo gnome-terminal --tab -e "docker run -ti --rm --net=host seresearch/opendavinci-ubuntu-16.04-complete:latest /opt/od4/bin/miniature/boxparker --cid=111 --freq=10"
+
+
 
 #xhost +
 
-#sudo docker run -ti --rm --net=host --ipc=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/DIT-168:/opt/configuration -w /opt/configuration seresearch/opendavinci-ubuntu-16.04-complete:latest /opt/od4/bin/odsimcamera --freq=10 --cid=111
+#sudo gnome-terminal --tab -e "docker run -ti --rm --net=host --ipc=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/DIT-168:/opt/configuration -w /opt/configuration seresearch/opendavinci-ubuntu-16.04-complete:latest /opt/od4/bin/odsimcamera --freq=10 --cid=111"
 
